81c7ec1943f72d90ca0a01f010bbf630cd1d2614
Kevin Ospina <kevinospina03mail.com>
2020-05-12 14:31:57 -0500

    1.3.0

--- meejel/admin.py
+++ meejel/admin.py
@@ -1,3 +1,22 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from .models import *
 
-# Register your models here.
+admin.site.site_header = 'Meejel Administration'
+admin.site.index_title = 'MEEJEL'
+admin.site.site_title = 'Meejel Admin'
+
+
+class AssessmentAdmin(admin.ModelAdmin):
+    model = Assessment
+    list_display = ('name', 'owner')
+    list_filter = ('name', 'owner')
+
+
+class PrincipleAdmin(admin.ModelAdmin):
+    model = Principle
+    list_display = ('principle', 'grade', 'justification')
+    list_filter = ('principle', 'grade')
+
+
+admin.site.register(Assessment, AssessmentAdmin)
+admin.site.register(Principle, PrincipleAdmin)
---
+++ meejel/serializers.py
@@ -0,0 +1,31 @@
+from .models import *
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'groups')
+
+
+class GroupSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('name', )
+
+
+class AssessmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Assessment
+        fields = ('name', 'owner', 'principles')
+
+    def to_representation(self, instance):
+        response = super().to_representation(instance)
+        response['principles'] = PrincipleSerializer(instance.principles, many=True).data
+        return response
+
+
+class PrincipleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Principle
+        fields = ('principle', 'grade', 'justification')
