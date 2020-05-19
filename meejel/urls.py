94f5a045d871ac6bd1cfa6ed227e4285e4e676ee
Kevin Ospina <kevinospina03mail.com>
2020-05-19 17:27:18 -0500

    1.5

--- meejel/urls.py
+++ meejel/urls.py
@@ -1,23 +1,26 @@
 from django.urls import path, include
 from rest_framework_nested import routers
-from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 
 from meejel.views import *
 
 router = routers.DefaultRouter()
 
+router.register('instrument', InstrumentViewSet, basename='instrument')
 router.register('assessment', AssessmentViewSet, basename='assessment')
+
+instrument_router = routers.NestedSimpleRouter(router, 'instrument', lookup='instrument')
+
 assessment_router = routers.NestedSimpleRouter(router, 'assessment', lookup='assessment')
 assessment_router.register('principle', PrincipleViewSet, base_name='principles')
-assessment_router.register('component', ComponentViewSet, base_name='components')
-
-principle_router = routers.NestedSimpleRouter(assessment_router, 'principle', lookup='principle')
-principle_router.register('evidence', EvidenceViewSet, base_name='evidences')
+assessment_router.register('evidence', EvidenceViewSet, base_name='evidences')
 
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(instrument_router.urls)),
     path('', include(assessment_router.urls)),
-    path('', include(principle_router.urls)),
     path('api-token-auth/', obtain_jwt_token),
+    path('api-token-refresh/', refresh_jwt_token),
+    path('api-token-verify/', verify_jwt_token),
 ]
--- meejel/views.py
+++ meejel/views.py
@@ -1,6 +1,8 @@
 import logging
+import copy
 
-from rest_framework import pagination
+from django.db.utils import IntegrityError
+from rest_framework import pagination, status
 from rest_framework import viewsets
 from rest_framework.response import *
 
@@ -25,11 +27,48 @@ class PaginationStandard(pagination.PageNumberPagination):
         })
 
 
-class AssessmentViewSet(viewsets.ModelViewSet):
-    serializer_class = AssessmentSerializer
+class InstrumentViewSet(viewsets.ModelViewSet):
+    serializer_class = InstrumentSerializer
+    # pagination_class = PaginationStandard
 
     def get_queryset(self):
-        return self.request.user.assessments.all()
+        return self.request.user.instruments.all()
+
+    def create(self, request, *args, **kwargs):
+        if self.request.user.is_anonymous:
+            return Response({'error': 'you are not logged in'}, status=status.HTTP_401_UNAUTHORIZED)
+        try:
+            name = request.data['name']
+            goals = request.data['Objetivos']
+            rules = request.data['Reglas']
+            roles = request.data['Roles']
+            steps = request.data['Pasos']
+            materials = request.data['Materiales']
+        except KeyError:
+            return Response({'error': 'missing fields'}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            new_instrument = Instrument.objects.create(name=name, owner=self.request.user)
+            for i in goals:
+                Component.objects.create(component_type='Objetivos', description=i['Oname'], instrument=new_instrument)
+            for i in rules:
+                Component.objects.create(component_type='Reglas', description=i['Rname'], instrument=new_instrument)
+            for i in roles:
+                Component.objects.create(component_type='Roles', description=i['Roname'], instrument=new_instrument)
+            for i in steps:
+                Component.objects.create(component_type='Pasos', description=i['Sname'], instrument=new_instrument)
+            for i in materials:
+                Component.objects.create(component_type='Materiales', description=i['Maname'], instrument=new_instrument)
+            return Response({'ok': 'created'}, status=status.HTTP_201_CREATED)
+        except IntegrityError:
+            return Response({'error': 'an instrument with that name already exists'}, status=status.HTTP_409_CONFLICT)
+
+
+class AssessmentViewSet(viewsets.ModelViewSet):
+    serializer_class = AssessmentSerializer
+    # pagination_class = PaginationStandard
+
+    def get_queryset(self):
+        return Assessment.objects.filter(instrument__owner=self.request.user)
 
 
 class PrincipleViewSet(viewsets.ModelViewSet):
@@ -40,21 +79,17 @@ class PrincipleViewSet(viewsets.ModelViewSet):
         queryset = Principle.objects.filter(assessment_id=assessment_id)
         return queryset
 
-
-class ComponentViewSet(viewsets.ModelViewSet):
-    serializer_class = ComponentSerializer
-
-    def get_queryset(self):
+    def create(self, request, *args, **kwargs):
         assessment_id = self.kwargs['assessment_pk']
-        queryset = Component.objects.filter(assessment_id=assessment_id)
-        return queryset
+        new_principle = Principle.objects.create(principle=request.data['principle'], grade=request.data['grade'],
+                                                 justification=request.data['justification'], assessment_id=assessment_id)
+        return Response(self.serializer_class(new_principle).data, status=status.HTTP_200_OK)
 
 
 class EvidenceViewSet(viewsets.ModelViewSet):
     serializer_class = EvidenceSerializer
 
     def get_queryset(self):
-        print(self.kwargs)
-        principle_id = self.kwargs['principle_pk']
-        queryset = Evidence.objects.filter(principle_id=principle_id)
+        assessment = self.kwargs['assessment_pk']
+        queryset = Evidence.objects.filter(principle__assessment=assessment)
         return queryset
